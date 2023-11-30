587a7ba5bfeaecd797773b99f3e6c2bda185f938
wojnet <wojnetto@gmail.com>
2023-11-30 19:58:28 +0100

    Added automatic user creation in mongodb when new user

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,6 +1,8 @@
 import type { NextAuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
-import CredentialsProvider from "next-auth/providers/credentials";
+import clientPromise from "@lib/mongo/clientPromise";
+
+
 
 export const options: NextAuthOptions = {
   providers: [
@@ -8,33 +10,35 @@ export const options: NextAuthOptions = {
       clientId: process.env.GITHUB_ID as string || "",
       clientSecret: process.env.GITHUB_SECRET as string || ""
     }),
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        username: {
-          label: "Username:",
-          type: "text",
-          placeholder: "your username"
-        },
-        password: {
-          label: "Password:",
-          type: "password",
-          placeholder: "your password"
-        }
-      },
-      async authorize(credentials) {
-        const user = { id: 42, name: "Dave", password: "nextauth" }
+  ],
+  callbacks: {
+    async jwt({ token, user, account, profile, trigger }) {
+      const client = await clientPromise;
+      const db = client.db("songwritingApp");
+      const collection = db.collection("userData");
+      
+      if (trigger === "signIn" || trigger === "signUp") {
+        const count = await collection.countDocuments({ 
+          userId: user.id
+        });
 
-        if (
-          credentials?.username === user.name &&
-          credentials?.password === user.password 
-        ) {
-          return user;
-        } else {
-          return null;
+        if (count === 0) {
+          let newUserData = {
+            userId: user.id,
+            name: user.name,
+            role: "user",
+            projects: { data: [] }
+          }
+  
+          await collection.insertOne(newUserData);
         }
       }
-    })
-  ],
 
+      // if (user?.id) {
+      //   token.userId = user.id;
+      // }
+
+      return token;
+    }
+  }
 }
